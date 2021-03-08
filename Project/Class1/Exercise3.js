d92d3220b70f7fc65536d14f0c04b819bd622a91
FELIPE BOTERO <60394674+fbotero1956@users.noreply.github.com>
2021-03-08 09:39:19 -0500

    Delete Exercise3.js

--- Project/Class1/Exercise3.js
+++
@@ -1,8 +0,0 @@
-let username=prompt("enter name");
-let useremail= prompt("enter email:");
-let msalary=prompt("Enter monthly salary");
-
-document.write(`Name: ${username} <br> email: ${useremail} <br>  Annual Salary: $${msalary * 12}`);
-
-
-
