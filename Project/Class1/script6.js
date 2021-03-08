d49c80294f5f4630d7e43a555817ec884653d932
FELIPE BOTERO <60394674+fbotero1956@users.noreply.github.com>
2021-03-08 09:40:28 -0500

    Delete script6.js

--- Project/Class1/script6.js
+++
@@ -1,8 +0,0 @@
-var names=["sam",20, 30.5, true, "hello"]
-console.table(names);
-console.log(names[0]);
-
-console.log(names.length);
-
-var inames=["sam",20, 30.5, true, "hello", names];
-console.table(inames);
