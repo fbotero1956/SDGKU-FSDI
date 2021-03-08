53ef876672a367da5240946af9b14ef8bc40da11
FELIPE BOTERO <60394674+fbotero1956@users.noreply.github.com>
2021-03-08 09:40:44 -0500

    Delete script8.js

--- Project/Class1/script8.js
+++
@@ -1,5 +0,0 @@
-function writetowebpage(){
-document.write(`<p class=orderalert> Alert, Purchasing Department:  ${userprompt} wants to purchase  ${orderquantity}  ${productname}(s) for a price of: ${productprice} each. </P>`);
-
-document.write(`<p class=ordertotal>Total price for this order including ${salestax}% is: ${Number(productprice) * Number(orderquantity) * (salestax + 1)} </p>`);
-}
